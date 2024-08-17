c040a9a518643be7d1733aac8a9a06f05fead2af
limpid-kzonix <balyszyn@gmail.com>
2024-08-17 03:13:41 +0300

    Add simple implementation of ESP32-CAM client
    Client sends the images as the image/jpeg (compressed jpeg) via HTTP to the server

--- src/main.cpp
+++ src/main.cpp
@@ -12,81 +12,165 @@
 #include "ArduinoHttpClient.h"
 #include "ArduinoJson.h"
 
+#define CAMERA_MODEL_AI_THINKER
+
 const char ssid[] = "<SSID>"; // your network SSID (name)
-const char pass[] = "<password>"; // your network password
+const char pass[] = "<PASSWORD>"; // your network password
 
-char serverAddress[] = "cat-fact.herokuapp.com"; // server address
-int port = 443;
+char serverAddress[] = "192.168.1.187"; // server address
+int port = 5000;
 
-String url = "/facts/random?animal_type=cat&amount=3";
-WiFiClientSecure networkClient;
+String url = "/upload";
+WiFiClient networkClient;
 HttpClient client = HttpClient(networkClient, serverAddress, port);
+
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+void configure_camera()
+{
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.grab_mode = CAMERA_GRAB_LATEST;
+
+  // camera init
+  // init with high specs to pre-allocate larger buffers
+  if (psramFound())
+  {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10; // 0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+  else
+  {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12; // 0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+};
+
+void initWifi()
+{
+  WiFi.begin(ssid, pass);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println("\n");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+}
 
 void setup()
 {
   Serial.begin(115200);
-  WiFi.begin(ssid, pass);
-
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(100);
-    Serial.print(".");
-  }
-  Serial.println();
-
-  networkClient.setInsecure();
-
-  Serial.println("Connected to WiFi");
+  configure_camera();
+  initWifi();
+  // networkClient.setInsecure();
 }
 
-void loop()
+void tryWifiReconnect()
 {
-  Serial.print("Making GET request: ");
-  Serial.print(serverAddress);
-  Serial.println(url);
-  // send GET request
-  client.beginRequest();
-  client.sendHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0");
-  client.sendHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
-  client.sendHeader("Content-Type", "application/json");
-  client.sendHeader("Host", "cat-fact.herokuapp.com");
-  client.sendHeader("Accept-Encoding", "gzip, deflate, br");
-  int result = client.get(url);
-  client.endRequest();
-
-  if (result == 0)
+  unsigned long currentMillis = millis();
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval))
   {
-    Serial.println("Response received");
-    int statusCode = client.responseStatusCode();
-    Serial.print("Status code: ");
-    Serial.println(statusCode);
-    String response = client.responseBody();
-
-    JsonDocument responseJson;
-    DeserializationError error = deserializeJson(responseJson, response);
-
-    if (error)
-    {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.f_str());
-      Serial.println(response);
-    }
-
-    JsonArray catsFacts = responseJson.as<JsonArray>();
-    for (JsonVariant catsFact : catsFacts)
-    {
-      Serial.println(catsFact["text"].as<String>());
-    }
+    Serial.print(millis());
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    previousMillis = currentMillis;
   }
   else
   {
-    Serial.print("Error on HTTP request: ");
-    Serial.println(result);
+    Serial.println("Wifi connection is stable...");
   }
-  client.stop();
+}
 
-  // read the status code and body of the response
-  Serial.println("Waiting 10 seconds before making a new request...");
-  delay(10000);
+void takePhotoAndSendToApi()
+{
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (!fb)
+  {
+    Serial.println("Camera capture failed");
+  }
+  else
+  {
+    client.beginRequest();
+    int result = client.post(url);
+    client.sendHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0");
+    client.sendHeader("Content-Type", "image/jpeg");
+    client.sendHeader("Content-Length", fb->len);
+    
+    if (result != 0) {
+      Serial.println("Tried to send initial headers and failed to establish connection");
+      esp_camera_fb_return(fb);
+      return;
+    }
+    client.beginBody();
+    client.write(fb->buf, fb->len);
+    client.endRequest();
+
+    int statusCode = client.responseStatusCode();
+    String response = client.responseBody();
+    Serial.println("Status code: " + String(statusCode));
+    Serial.println("Response: " + response);
+    esp_camera_fb_return(fb);
+  }
+}
+void loop()
+{
+  tryWifiReconnect();
+  takePhotoAndSendToApi();
 }
